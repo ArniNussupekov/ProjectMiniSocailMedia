0411fc85cf0b4579f76aaaabd79bddc6f6884847
Arnibek Nussupekov <arnibekcs@gmail.com>
2022-10-23 21:00:59 +0600

    c11

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,7 @@ import java.util.HashMap;
 import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
-
+        UserInterface processing = new UserInterface();
+        processing.mainProcess();
     }
 }
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,7 +1,43 @@
 import java.util.HashMap;
+import java.util.Scanner;
 
 class UserInterface {
    private boolean isLogged;
    private Server server;
    private User loggedUser;
+   private int choice = 0;
+   private Scanner in;
+
+      UserInterface(){
+         isLogged = false;
+         server = new Server();
+         in = new Scanner(System.in);
+      }
+
+      public void mainProcess(){
+       printMainMenu();
+
+        while (choice != 3) {
+            choice = in.nextInt();
+               if (choice == 1){
+                 logIn();
+               }
+               if (choice == 2){
+                 server.register();
+               }
+               if (choice == 3){
+                  System.exit(0);
+               }
+        }
+      }
+      public void logIn(){
+         String number = in.nextLine();
+         String password = in.nextLine();
+          loggedUser = server.getUser(number, password);
+          isLogged = true;
+      }
+   public void printMainMenu(){
+      System.out.println("Welcome to social media! This is main menu. Please, choose what to you want");
+      System.out.println("1. Log in \n2. Register \n3. Exit\n");
+   }
 }
