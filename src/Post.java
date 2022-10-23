e5037614f06c12a1f3d29bd58e255c005e0bee30
Arnibek Nussupekov <arnibekcs@gmail.com>
2022-10-23 19:15:05 +0600

    c7

--- src/Post.java
+++ src/Post.java
@@ -1,2 +1,37 @@
-public class Post {
+import java.util.LinkedList;
+
+class Post {
+    public String post;
+    private int like;
+    public LinkedList<String> comments;
+
+    Post(String post){
+        this.post = post;
+        like = 0;
+        comments = new LinkedList<>();
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public void setPost(String post) {
+        this.post = post;
+    }
+
+    public int getLike() {
+        return like;
+    }
+
+    public void setLike(int like) {
+        this.like = like;
+    }
+
+    public LinkedList<String> getComments() {
+        return comments;
+    }
+
+    public void setComments(LinkedList<String> comments) {
+        this.comments = comments;
+    }
 }
